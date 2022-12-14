d2002bacea61d27747ded3bcd475b330b8369584
hiraaziz <hiraaziz1998@gmail.com>
2022-12-14 22:02:32 +0500

    Create CashWithdraw.js

---
+++ atm/bin/CashWithdraw.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+export async function cash_withdraw(balance) {
+    let withdraw = await inquirer
+        .prompt([
+        {
+            type: "input",
+            name: "amount",
+            message: chalk.yellowBright(`\n Your Current Balance is ${balance}. Please enter the withdrawal amount : `),
+            validate(answers) {
+                if (answers > balance) {
+                    console.log(chalk.redBright(' \n Enter amount less than'));
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+    ]).then((ans) => {
+        console.log(chalk.greenBright(`\t \t ${ans.amount} has been withdrawn`));
+        return ans;
+    });
+    return withdraw.amount;
+}
