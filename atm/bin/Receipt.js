4ef3aa7083aabf2de2b2cd5a7b9824777a0be01f
hiraaziz <hiraaziz1998@gmail.com>
2022-12-14 22:04:43 +0500

    Create Receipt.js

---
+++ atm/bin/Receipt.js
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+export async function Recipt(username, deduct_amount, balance) {
+    let pin = await inquirer
+        .prompt([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: chalk.yellowBright("Do you want Receipt?: "),
+        }
+    ]).then((ans) => {
+        if (ans.confirm == true) {
+            console.log(chalk.greenBright(`\n\tAccount Name : ${username}`));
+            console.log(chalk.greenBright(`\tdeduct_amount Amount : ${deduct_amount}`));
+            console.log(chalk.greenBright(`\tRemaining Balance : ${balance - deduct_amount}\n`));
+        }
+    });
+    return pin;
+}
