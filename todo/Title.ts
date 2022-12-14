117d27682a81ba92a39b5e122423e04bfdf04324
hiraaziz <hiraaziz1998@gmail.com>
2022-12-14 22:04:03 +0500

    Create Title.ts

---
+++ todo/Title.ts
@@ -0,0 +1,14 @@
+import figlet from 'figlet'
+import gradient from 'gradient-string'
+
+export async function title(){
+
+    console.log(gradient.morning(figlet.textSync('TODO APP', {
+        font: 'Banner3',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true
+    })));
+
+}
