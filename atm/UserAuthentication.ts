205463c1fc2366a6ddd7f42b1dd565735701816e
hiraaziz <hiraaziz1998@gmail.com>
2022-12-14 22:04:24 +0500

    Create UserAuthentication.ts

---
+++ atm/UserAuthentication.ts
@@ -0,0 +1,33 @@
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+type pass = {
+    password:string
+}
+// Getting pin from user
+export async function userAuthentication(){
+        let username:string = ''
+        let pin = await inquirer
+        .prompt([
+            {
+                type:"input",
+                name:"username",
+                message:chalk.yellowBright("Enter your name : ")
+            },
+            {
+                type:"password",
+                name:"password",
+                message:chalk.yellowBright("Enter your 4 digit Pin : "),
+                validate(answers){
+                    if(answers.length !== 4){
+                        console.log(chalk.redBright(' \n Please Enter 4 Digit Pin'))
+                    }else{
+                        return true
+                    }
+                }
+            }
+        ]).then((ans)=>{
+            username = ans.username
+        })
+        return username
+}
