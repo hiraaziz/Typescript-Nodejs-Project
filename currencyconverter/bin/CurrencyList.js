634343a1266e099b9a95e334336c505b6eafbf58
hiraaziz <hiraaziz1998@gmail.com>
2022-12-15 17:24:12 +0500

    Create CurrencyList.js

---
+++ currencyconverter/bin/CurrencyList.js
@@ -0,0 +1,62 @@
+export const currencylist = [
+    {
+        country: 'Pakistan',
+        currency_code: 'PKR',
+        value: 223
+    },
+    {
+        country: 'USA',
+        currency_code: 'USD',
+        value: 1
+    },
+    {
+        country: 'UK',
+        currency_code: 'GBP',
+        value: 0.81
+    },
+    {
+        country: 'Canada',
+        currency_code: 'CAD',
+        value: 1.36
+    },
+    {
+        country: 'UAE',
+        currency_code: 'AED',
+        value: 3.67
+    },
+    {
+        country: 'Saudia Arab',
+        currency_code: 'SAR',
+        value: 3.76
+    },
+    {
+        country: 'Turkey',
+        currency_code: 'TRY',
+        value: 18.63
+    },
+    {
+        country: 'Russia',
+        currency_code: 'RUB',
+        value: 64.70
+    },
+    {
+        country: 'Japan',
+        currency_code: 'JPY',
+        value: 135.53
+    },
+    {
+        country: 'India',
+        currency_code: 'INR',
+        value: 82.63
+    },
+    {
+        country: 'Europe',
+        currency_code: 'EUR',
+        value: 0.94
+    },
+    {
+        country: 'China',
+        currency_code: 'CNY',
+        value: 6.95
+    }
+];
