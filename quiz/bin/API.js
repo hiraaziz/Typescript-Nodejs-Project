46069095f6ddaa167a0cdc5a6ff173fd9c8679a2
hiraaziz <hiraaziz1998@gmail.com>
2022-12-27 11:57:58 +0500

    Create API.js

---
+++ quiz/bin/API.js
@@ -0,0 +1,9 @@
+export const fetchQuizQuestions = async () => {
+    const fetch_data = await fetch('https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple');
+    const data = await fetch_data.json();
+    return await data.results.map((question) => ({
+        question: question.question,
+        correct_answer: question.correct_answer,
+        incorrect_answers: question.incorrect_answers
+    }));
+};
