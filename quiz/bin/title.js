77e1322c46931b3d005d37b66e832a115bcec070
hiraaziz <hiraaziz1998@gmail.com>
2022-12-27 11:58:07 +0500

    Create title.js

---
+++ quiz/bin/title.js
@@ -0,0 +1,17 @@
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 1000);
+    });
+};
+export async function title() {
+    console.log(gradient.teen(figlet.textSync('Currency Converter APP', {
+        font: 'Banner3',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 150,
+        whitespaceBreak: true
+    })));
+    sleep();
+}
